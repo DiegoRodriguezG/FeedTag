d30073cf8fa731e18b941cee091649b0b0a71275
Diego Rodriguezg <diego@MacBook-Pro-de-Diego.local>
2014-03-22 16:30:38 -0300

    Root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Feedtag::Application.routes.draw do
+  root "public#index"
+  get "public/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
