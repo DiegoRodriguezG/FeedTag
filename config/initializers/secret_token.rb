6f9f48e9cd035b5285aa160346e2458ddf0bdaa5
Diego Rodriguezg <diego@MacBook-Pro-de-Diego.local>
2014-03-22 00:48:05 -0300

    Nuevo Proyecto FeedTag!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Feedtag::Application.config.secret_key_base = 'ad20701f4e2a4902804df7a3a83cbae083caa9ec9d51332b035dbbf3978cf6aefe8af4bd03b4bfa5f9c987b03db560667cfcaaabdcbfb2d501e3ce589a7d7f53'
