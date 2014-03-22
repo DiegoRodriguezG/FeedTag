c9299139ae9079fe9c70e765c25ed1fc1d02b4da
Diego Rodriguezg <diego@MacBook-Pro-de-Diego.local>
2014-03-22 17:53:15 -0300

    Modelo Categorías.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140322153638) do
+ActiveRecord::Schema.define(version: 20140322205138) do
+
+  create_table "categoria", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "productos", force: true do |t|
     t.datetime "created_at"
