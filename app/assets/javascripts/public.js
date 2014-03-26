8efa688c554c17f39b0121659d0a57388fd17ed4
Diego Rodriguezg <diego@MacBook-Pro-de-Diego.local>
2014-03-26 08:46:03 -0300

    public.js con los tomen inputs, funciona si se descomenta, pero se ve muy grande.

---
+++ app/assets/javascripts/public.js
@@ -0,0 +1,11 @@
+Index = {
+  buscar: function() {
+  	// $("#token-input").tokenInput("productos_tokens" ,{
+   //    propertyToSearch: "nombre",
+   //    preventDuplicates: true,
+   //    resultsLimit: 2,
+   //    theme: 'facebook',
+   //    hintText: 'Escribe el nombre del producto'
+   //  });
+  }
+}
