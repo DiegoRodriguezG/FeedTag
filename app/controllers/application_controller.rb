98a9cbda8478b400f1f66792ea0a33a8a7504b06
Diego Rodriguezg <diego@MacBook-Pro-de-Diego.local>
2014-03-26 21:03:09 -0300

    método current_user, donde se crea la session de el usuario, por su id.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def current_user
+  	@current_user ||= Usuario.find(session[:id]) if session[:id]
+  end
+  helper_method :current_user
 end
